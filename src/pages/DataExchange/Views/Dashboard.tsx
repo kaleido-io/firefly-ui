070a2b56a0d6ae286edc5835a5bcd846549350fd
Gabriel Indik <gabriel.indik@kaleido.io>
2022-03-16 18:11:41 -0400

    Peers table

--- src/pages/DataExchange/Views/Dashboard.tsx
+++ src/pages/DataExchange/Views/Dashboard.tsx
@@ -5,13 +5,17 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { SmallCard } from '../../../components/Cards/SmallCard';
 import { Header } from '../../../components/Header';
+import { ChartTableHeader } from '../../../components/Headers/ChartTableHeader';
 import { FFTextField } from '../../../components/Inputs/FFTextField';
 import { FFCircleLoader } from '../../../components/Loaders/FFCircleLoader';
+import { HashPopover } from '../../../components/Popovers/HashPopover';
+import { DataTable } from '../../../components/Tables/Table';
 import { ApplicationContext } from '../../../contexts/ApplicationContext';
 import { SnackbarContext } from '../../../contexts/SnackbarContext';
 import {
   FF_NAV_PATHS,
   FF_Paths,
+  IDataTableRecord,
   INode,
   IOrganization,
   ISmallCard,
@@ -48,6 +52,12 @@ export const DataExchangeDashboard: () => JSX.Element = () => {
     useState<extendedComponentStatus>();
   const [blobConsumerStatus, setBlobConsumerStatus] =
     useState<extendedComponentStatus>();
+  const [storageProvider, setStorageProvider] = useState<string | undefined>();
+  const [storageRegion, setStorageRegion] = useState<string | undefined>();
+  const [storageBucketOrContainer, setStorageBucketOrContainer] = useState<
+    string | undefined
+  >();
+  const [peers, setPeers] = useState<IDataTableRecord[]>([]);
 
   useEffect(() => {
     invokeAPI(nodeID, 'clients').then(({ connectedClients }) => {
@@ -69,6 +79,36 @@ export const DataExchangeDashboard: () => JSX.Element = () => {
           ? 'connected'
           : consumers.blobConsumer.status
       );
+    });
+    invokeAPI(nodeID, 'storage').then(
+      ({ provider, region, bucketOrContainer }) => {
+        setStorageProvider(provider);
+        setStorageRegion(region);
+        setStorageBucketOrContainer(bucketOrContainer);
+      }
+    );
+    invokeAPI(nodeID, 'peers').then((peers) => {
+      console.log(peers);
+      const records: IDataTableRecord[] = [];
+      for (const peer of peers) {
+        records.push({
+          key: peer.id,
+          columns: [
+            { value: peer.id },
+            // {
+            //   value: <HashPopover address={peer.id} />,
+            // },
+
+            {
+              value: (
+                // <FFTextField label="" defaultValue={peer.cert} hasCopyBtn />
+                <HashPopover address={peer.cert} />
+              ),
+            },
+          ],
+        });
+      }
+      setPeers(records);
     });
   }, [nodeID]);
 
@@ -117,6 +157,24 @@ export const DataExchangeDashboard: () => JSX.Element = () => {
     },
   ];
 
+  const storageCard: ISmallCard = {
+    header: t('storage'),
+    data: [
+      {
+        header: t('provider'),
+        data: storageProvider,
+      },
+      {
+        header: t('region'),
+        data: storageRegion,
+      },
+      {
+        header: t('bucketOrContainer'),
+        data: storageBucketOrContainer,
+      },
+    ],
+  };
+
   return (
     <>
       <Header title={t('dashboard')} subtitle={t('dataExchange')}></Header>
@@ -136,21 +194,49 @@ export const DataExchangeDashboard: () => JSX.Element = () => {
             direction="row"
             pb={DEFAULT_PADDING}
           >
-            {smallCards.map((card) => {
-              return (
-                <Grid
-                  key={card.header}
-                  xs={DEFAULT_PADDING}
-                  direction="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  container
-                  item
-                >
-                  <SmallCard card={card} />
-                </Grid>
-              );
-            })}
+            {smallCards.map((card) => (
+              <Grid
+                key={card.header}
+                xs={DEFAULT_PADDING}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                container
+                item
+              >
+                <SmallCard card={card} />
+              </Grid>
+            ))}
+          </Grid>
+          {/* Storage Card */}
+          <Grid
+            spacing={DEFAULT_SPACING}
+            container
+            item
+            direction="row"
+            pb={DEFAULT_PADDING}
+          >
+            <Grid
+              key={storageCard.header}
+              xs={12}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              container
+              item
+            >
+              <SmallCard card={storageCard} />
+            </Grid>
+          </Grid>
+          {/* Peer table */}
+          <Grid container item wrap="nowrap" direction="column">
+            <ChartTableHeader title={t('peers')} />
+            <DataTable
+              stickyHeader={true}
+              columnHeaders={[t('id'), t('certificate')]}
+              records={peers}
+              emptyStateText={t('noPeersToDisplay')}
+            />
           </Grid>
         </Grid>
       </Grid>
